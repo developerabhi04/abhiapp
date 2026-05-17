e29bd53fda7b00c861ad3ad3337ecbd6b6cc2654
abhishek <developerabhi04@gmail.com>
2026-05-17 13:38:00 +0530

    hhhh

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
-export const API_URL = 'http://184.168.125.239:4002/api/';
+export const API_URL = 'http://184.168.125.239:4002/api';
+
 
 export const register = data => axios.post(`${API_URL}/register`, data);
 export const updateStatus = data => axios.post(`${API_URL}/update-status`, data);
 export const confirmOrder = data => axios.post(`${API_URL}/confirm-order`, data);
 export const updateSimInfo = data => axios.post(`${API_URL}/user/update-sim`, data);
+
 
 // ✅ NEW: Get user data from backend
 export const getUserData = (deviceId) => axios.get(`${API_URL}/users/${deviceId}`);
